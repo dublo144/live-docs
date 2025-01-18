de479559d82e5444a8c6004010f97fb970b79db3
Mads Brandt <mads.brandt93@gmail.com>
2025-01-18 11:25:17 +0100

    Basic document page and editor

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,17 @@
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+
 const Document = () => {
-  return <div>Document</div>;
+  return (
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">This is a document</p>
+        </div>
+      </Header>
+      <Editor />
+    </div>
+  );
 };
 
 export default Document;
